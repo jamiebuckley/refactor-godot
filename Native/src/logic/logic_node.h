443bdf5fa69273cdd472d2e097fd8bbaaa395d8a
Jamie Buckley <jbuckley760@gmail.com>
2019-07-31 16:20:27 +0100

    Added more logic node types

--- Native/src/logic/logic_node.h
+++ Native/src/logic/logic_node.h
@@ -16,6 +16,9 @@ namespace Refactor {
         DATA_WORKER_IS,
         DATA_COUNTER,
         NUMERICAL_COMPARISON,
+        NUMBER,
+        WORKER_TYPE,
+        INVENTORY_ITEM
     };
 
     struct NamedLogicNodeConnection {
@@ -24,29 +27,81 @@ namespace Refactor {
     };
 
     struct LogicNodeType {
-      std::vector<NamedLogicNodeConnection> connections_out;
-      std::vector<NamedLogicNodeConnection> connections_in;
+        std::vector<NamedLogicNodeConnection> connections_out;
+        std::vector<NamedLogicNodeConnection> connections_in;
     };
 
+
+// Define shorthands to reduce verbosity here
+#define NLNC NamedLogicNodeConnection
+#define LNC LogicNodeConnection
+
     class LogicNodeTypes {
+    private:
+        LogicNodeTypes() = default;
+
+        ~LogicNodeTypes() = default;
+
     public:
-        static LogicNodeType* AND() {
-          static LogicNodeType* _AND = new LogicNodeType {
-                  .connections_out =  { NamedLogicNodeConnection { "Out", LogicNodeConnection::BOOLEAN } },
-                  .connections_in = { NamedLogicNodeConnection { "Value 1 In", LogicNodeConnection::BOOLEAN }, NamedLogicNodeConnection { "Value 2 In", LogicNodeConnection::BOOLEAN } }
-          };
-          return _AND;
+        const LogicNodeType* AND = new LogicNodeType{
+                .connections_out =  {NLNC{"Out", LNC::BOOLEAN}},
+                .connections_in = {
+                        NLNC{"Value 1 In", LNC::BOOLEAN},
+                        NLNC{"Value 2 In", LNC::BOOLEAN}
+                }
         };
+
+        const LogicNodeType *OR = new LogicNodeType{
+                .connections_out =  {NLNC{"Out", LNC::BOOLEAN}},
+                .connections_in = {
+                        NLNC{"Value 1 In", LNC::BOOLEAN},
+                        NLNC{"Value 2 In", LNC::BOOLEAN}
+                }
+        };
+
+        const LogicNodeType *WORKER_IS = new LogicNodeType{
+                .connections_out =  {NLNC{"Out", LNC::BOOLEAN}},
+                .connections_in = {NLNC{"Value is", LNC::NUMERICAL_COMPARISON}}
+        };
+
+        const LogicNodeType *COUNTER_IS = new LogicNodeType{
+                .connections_out =  {NLNC{"Out", LNC::BOOLEAN}},
+                .connections_in = {NLNC{"Value is", LNC::NUMERICAL_COMPARISON}}
+        };
+
+        const LogicNodeType *NUMERICAL_EQUALS = new LogicNodeType{
+                .connections_out =  {NLNC{"Out", LNC::NUMERICAL_COMPARISON}},
+                .connections_in = {NLNC{"Value is", LNC::NUMBER}}
+        };
+
+        const LogicNodeType *NUMERICAL_GREATER_THAN = new LogicNodeType{
+                .connections_out =  {NLNC{"Out", LNC::NUMERICAL_COMPARISON}},
+                .connections_in = {NLNC{"Value is", LNC::NUMBER}}
+        };
+
+        const LogicNodeType *NUMERICAL_LESS_THAN = new LogicNodeType{
+                .connections_out =  {NLNC{"Out", LNC::NUMERICAL_COMPARISON}},
+                .connections_in = {NLNC{"Value is", LNC::NUMBER}}
+        };
+
+        static LogicNodeTypes *getInstance() {
+            static auto _internal = new LogicNodeTypes();
+            return _internal;
+        }
     };
+
+#undef NLNC
+#undef LNC
 
     class LogicNode {
     public:
-      bool stuff() {
-        auto thingy = LogicNodeTypes::AND();
-        auto thingy2 = LogicNodeTypes::AND();
-        return thingy == thingy2;
-      }
+        bool stuff() {
+            auto thingy = LogicNodeTypes::getInstance()->AND;
+            auto thingy2 = LogicNodeTypes::getInstance()->AND;
+            return thingy == thingy2;
+        }
     };
+
 }
 
 #endif //REFACTOR_NATIVE_LOGIC_NODE_H
