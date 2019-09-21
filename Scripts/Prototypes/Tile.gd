53b4337ed88935e29ea1891ae631f157b7383354
Jamie <jbuckley760@gmail.com>
2019-09-21 23:25:14 +0100

    POC of animated tile

--- Scripts/Prototypes/Tile.gd
+++ Scripts/Prototypes/Tile.gd
@@ -2,32 +2,28 @@ extends Node
 
 
 func _ready():
+	$AnimatedArrow/AnimationPlayer.play("Play")
 	pass
 
 func _set_selected():
-	var cube = get_node("Mesh").get_node("Cube")
-	var surface_material = cube.mesh.surface_get_material(0)
+	pass
+	#var cube = get_node("Mesh").get_node("Cube")
+	#var surface_material = cube.mesh.surface_get_material(0)
 	
-	var new_mat = SpatialMaterial.new()
-	new_mat.albedo_color =  Color(0.6, 0.9, 0.6, 1);
-	new_mat.albedo_texture = surface_material.albedo_texture
-	cube.set_surface_material(0, new_mat)
+	#var new_mat = SpatialMaterial.new()
+	#new_mat.albedo_color =  Color(0.6, 0.9, 0.6, 1);
+	#new_mat.albedo_texture = surface_material.albedo_texture
+	#cube.set_surface_material(0, new_mat)
 
 func _set_unselected():
-	var cube = get_node("Mesh").get_node("Cube")
-	cube.set_surface_material(0, null)
+	pass
+	#var cube = get_node("Mesh").get_node("Cube")
+	#cube.set_surface_material(0, null)
 
 func _set_disabled():
-	print("set disabled")
-	var cube = get_node("Mesh").get_node("Cube")
-	var surface_material = cube.mesh.surface_get_material(0)
-	
-	var new_mat = SpatialMaterial.new()
-	new_mat.albedo_color =  Color(0.2, 0.2, 0.2, 0.5);
-	new_mat.albedo_texture = surface_material.albedo_texture
-	cube.set_surface_material(0, new_mat)
+	$AnimatedArrow.hide()
+	$OfflineArrow.show()
 
 func _set_enabled():
-	print("set enabled")
-	var cube = get_node("Mesh").get_node("Cube")
-	cube.set_surface_material(0, null)
+	$AnimatedArrow.show()
+	$OfflineArrow.hide()
